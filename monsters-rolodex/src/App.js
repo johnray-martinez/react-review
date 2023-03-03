ac4a68690077bab9a25aaf536bdb93768e7a9b79
johnray-martinez <johnraymartinezwebdev@gmail.com>
2023-03-03 00:23:05 -0500

    updated class components to use hooks and functions

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,16 +1,14 @@
-import {Component} from 'react';
 import Dashboard from './pages/Dashboard';
 import './App.css';
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <Dashboard />
-        </header>
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Dashboard />
+      </header>
+    </div>
+  );
 }
 
 export default App;
